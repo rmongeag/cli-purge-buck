dd97c8712468a0630426bbb29a5522fefdf7a9d9
Roberto Monge <rmongeag@akamai.com>
2018-12-18 22:28:49 -0600

    test:wq

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cli-purge-buck
+# Akamai CLI: Python Example Command
+
+This repo is an _example_ command for Akamai CLI written in Python.
+
+## Install
+
+Installation is done via `akamai install`:
+
+```
+$ akamai install example-python
+```
+
+Running this will run the system `python setup.py` automatically. 
+
+## Updating
+
+To update to the latest version:
+
+```
+$ akamai update example-python
+```
--- bin/purgetestnew.py
+++ bin/purgetestnew.py
@@ -11,49 +11,6 @@ import argparse
 import logging
 from datetime import datetime
 
-
-class buck_purge(object):
-
-    print (sys.argv[0])
-    def __init__(self):
-        parser = argparse.ArgumentParser(
-            description='Purge or invalidate 50 URLs per request.',
-            usage='''akamai buck_purge <command> [<args>]
-
-The commands that can be executed are:
-    delete     delete the content from cache
-    invalidate      invalidate the content from cache
-''')
-        parser.add_argument('command', help='Subcommand to run')
-        # parse_args defaults to [1:] for args, but you need to
-        # exclude the rest of the args too, or validation will fail
-        args = parser.parse_args(sys.argv[1:2])
-        if not hasattr(self, args.command):
-            print ('Unrecognized command')
-            parser.print_help()
-            exit(1)
-        # use dispatch pattern to invoke method with same name
-        getattr(self, args.command)()
-
-    def invalidate(self):
-        parser = argparse.ArgumentParser(
-            description='Invalidate urls from cache in the file provided.')
-        # prefixing the argument with -- means it's optional
-        parser.add_argument('urls', action='store', help='File name with the urls to purge')
-        # now that we're inside a subcommand, ignore the first
-        # TWO argvs, ie the command (git) and the subcommand (commit)
-        args = parser.parse_args(sys.argv[2:])
-        print ('Running invalidate, urls=%s' % args.urls)
-
-    def delete(self):
-        parser = argparse.ArgumentParser(
-            description='delete urls from cache in the file provided.')
-        # NOT prefixing the argument with -- means it's not optional
-        parser.add_argument('urls', action='store', help='File name with the urls to purge')
-        args = parser.parse_args(sys.argv[2:])
-        print ('Running purge, urls=%s' % args.urls)
-        
-
 ### Adding Logging ###
 logger = logging.getLogger('purge-list')
 hdlr = logging.FileHandler('./' + datetime.now().strftime('purge_%H_%M_%d_%m_%Y.log'))
@@ -82,10 +39,11 @@ def purge_url(s, burl, purgeurl, netwrk, action):
         headers = {'content-type': 'application/json'}
         data = {'objects': purgeurl}
 
+        print (json.dumps(data))
+
         response = s.post(urljoin(burl, '/ccu/v3/' + action + '/url/' + netwrk), data=json.dumps(data), headers=headers)
-        print ('\nURLs to purge:')
         print (purgeurl)
-        print ('\nResponse:')
+        print ('\n')
         print (response)
         print ('\n')
 
@@ -105,17 +63,37 @@ def purge_url(s, burl, purgeurl, netwrk, action):
 
     return response
     
-#def do_purge(cmdargs):
+def do_purge(cmdargs):
+    
+    subparsers = parser.add_subparsers(help='commands', dest="command")
 
+    create_parser = subparsers.add_parser("help", help="Show available help")
+    create_parser = subparsers.add_parser("list", help="Subcommands")
+    create_parser = subparsers.add_parser("hello", help="A cheerful greeting")
     
+    opts = parser.parse_args()
+
+    if opts.command == "help":
+        parser.print_help()
+
+    elif opts.command == "list":
+        parser.print_help()
+        
+    elif opts.command == "hello":
+        print ("Hello %s" % opts.name)
+
+    else:
+        # argparse will error on unexpected commands, but
+        # in case we mistype one of the elif statements...
+        parser.print_help(sys.stderr)
+
 #    parser = argparse.ArgumentParser(description='Purge or invalidate 50 URLs per request.')
-    
 #    parser.add_argument('--section', action='store', required=True, help='.edgerc file section')
 #    parser.add_argument('--urls', action='store', required=True, help='File with the urls to purge')
 #    parser.add_argument('--network', action='store', required=True, help='Network to execute the purge/invalidate action.')
 #    parser.add_argument('--action', action='store', required=True, help='Action to excecute Delete or Invalidate')
     
-#   try:
+#    try:
 #        args = parser.parse_args(cmdargs)
 #        logger.addHandler(hdlr) 
 #    except SystemExit:
@@ -133,9 +111,8 @@ def purge_url(s, burl, purgeurl, netwrk, action):
 #            line= [x.strip() for x in islice(myfile, 50)]
             
 def main():
-    buck_purge()
-    #do_purge(sys.argv[1:])
+    
+    do_purge(sys.argv[1:])
     
 if __name__ == "__main__":
     main()
-    #buck_purge()
